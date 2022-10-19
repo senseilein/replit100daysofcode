22467171a9268ca82963c9b581fa6df1e6ebdbf7
Sansi <73607360+senseilein@users.noreply.github.com>
2022-10-19 15:51:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+type_of_food = input("Name a food: ")
+type_of_plant = input("Name a type of plant: ")
+cooking_method = input("Name a cooking method: ")
+burned = input("What word describes burned food? ")
+DIY_item = input("Name a DIY item: ")
+
+print(f"""
+MENU
+{type_of_food} with {burned} {type_of_plant} on a bed of {DIY_item}
+""")
